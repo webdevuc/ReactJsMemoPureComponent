2cf2033c6736978114305c548af9d2dfedcfdaf3
unknown <webdevelopersuc@gmail.com>
2022-06-29 15:40:18 +0530

    Made the PureComp

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import A from "./pages/A";
+import B from "./pages/B";
+import C from "./pages/C";
+import D from "./pages/D";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <A />
+      <B /> */}
+      <C/>
+      <D/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/C.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Todos from "./Todos";
+const C = () => {
+    const [count, setCount] = useState(0);
+    const [todos, setTodos] = useState([]);
+  
+    const increment = () => {
+      setCount((c) => c + 1);
+    };
+    const addTodo = () => {
+      setTodos((t) => [...t, "New Todo"]);
+    };
+  
+    return (
+      <>
+        <Todos todos={todos} addTodo={addTodo} />
+        <hr />
+        <div>
+          Count: {count}
+          <button onClick={increment}>+</button>
+        </div>
+      </>
+    );
+  };
+
+export default C
